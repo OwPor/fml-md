fb62b361d7974933f673e81006e169a0c0543c23
OwPor <rayvincentconcepcion@gmail.com>
2025-05-08 21:42:13 +0800

    Revert version number in setup.py from 0.1.1 to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fml-md",
-    version="0.1.1",
+    version="0.1.0",
     author="OwPor",
     author_email="concepcionrayvincent@gmail.com",
     description="Fibonacci Markup Language - where indentation follows the Fibonacci sequence",
